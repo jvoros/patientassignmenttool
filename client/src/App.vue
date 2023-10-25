bff43101cf66e421adb6c5250a5c361b890d4807
Jeremy Voros <jeremyvoros@gmail.com>
2023-10-25 10:58:16 -0600

    polish

--- client/src/App.vue
+++ client/src/App.vue
@@ -37,12 +37,12 @@ function toggleError() {
     <Navbar :role="store.user.role" @add-doctor="toggleAddDoctor" @logout="logout" />
     <AddDoctor v-if="addDoctorModal" @close="toggleAddDoctor"/>
     <Message severity="error" class="w-1/2 mx-auto" v-if='errorModal' @close="toggleError">{{  store.error.text }}</Message>
-    <div class="grid grid-cols-1 md:grid-cols-10 gap-10 px-4 my-8">
+    <div class="grid grid-cols-1 md:grid-cols-10 gap-12 px-4 my-8">
       <Timeline :events="store.board.timeline" class="order-3 md:order-1 md:col-span-3" />
       <BoardRotation pointer :rotation="store.board.rotations.main" class="order-1 md:order-2 md:col-span-4" />
       <div class="bg-gray-50 md:col-span-3 order-2 md:order-2">
-        <BoardRotation :rotation="store.board.rotations.ft" />
-        <BoardRotation :rotation="store.board.rotations.off" />
+        <BoardRotation :rotation="store.board.rotations.ft" variation="ft" />
+        <BoardRotation :rotation="store.board.rotations.off" variation="off" />
       </div>
     </div>
   </div>
--- client/src/components/BoardRotation.vue
+++ client/src/components/BoardRotation.vue
@@ -23,10 +23,19 @@ function isNext(shift_index) {
   return false;
 }
 
+function getStyles() {
+  const c = {
+    'default': 'bg-white border-gray-200',
+    'ft': 'bg-green-50 border-green-300 text-gray-600 hover:bg-green-50',
+    'off': 'text-gray-500'
+  }
+  return c[props.variation];
+}
+
 function countColor(count) {
   const c = {
-    'total': "bg-gray-100",
-    'ft': "bg-green-100",
+    'total': "bg-gray-200",
+    'ft': "bg-green-200",
     'walk-in': "bg-blue-50",
     'ambo': "bg-red-50"
   }
@@ -36,10 +45,13 @@ function countColor(count) {
 </script>
 <template>
   <BoardPanel :header="rotation.name + ' Rotation'">
-    <div v-for="shift, index in rotation.shifts" :class="[isNext(index) ? 'shadow-md border-4 border-amber-300' : 'border-gray-300']" class="border  my-6 rounded-md bg-white hover:bg-gray-50" >
+    <div v-for="shift, index in rotation.shifts"  
+      class="my-6 rounded-md hover:bg-gray-50 border" 
+      :class="[getStyles(), isNext(index) ? 'shadow-md border-2 !border-amber-200' : '']"
+    >
       <!-- flex: shift controls -->
-      <div class="flex justify-between py-2 px-4 rounded-t items-center p-1 bg-gray-100">
-        <div>
+      <div class="flex py-2 px-4 rounded-t items-center p-1 bg-gray-100" :class="[pointer ? 'justify-between' : 'justify-end']">
+        <div v-if="pointer">
           <button class="px-2 py-1 rounded-s bg-gray-100 border border-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" width="12" height="12"><path d="M6 4c-.2 0-.4.1-.5.2L2.2 7.5c-.3.3-.3.8 0 1.1.3.3.8.3 1.1 0L6 5.9l2.7 2.7c.3.3.8.3 1.1 0 .3-.3.3-.8 0-1.1L6.6 4.3C6.4 4.1 6.2 4 6 4Z"></path></svg>
           </button>
@@ -54,6 +66,7 @@ function countColor(count) {
           </select>
         </div>
       </div>
+
       <!-- flex: shift details and next pill -->
       <div class="flex items-center px-6 py-4">
         <!-- shift details box -->
@@ -62,8 +75,10 @@ function countColor(count) {
           <h4 class="font-semibold text-2xl">{{ shift.doctor.first }} {{ shift.doctor.last }}</h4>
         </div>
         <!-- next pill box -->
-        <div v-if="isNext(index)" class="rounded-full px-4 py-2 bg-amber-300 text-yellow-50">Next<i class="ml-2 text-xl fa-solid fa-star"></i></div>
+        <div v-if="isNext(index)" class="rounded-full px-4 py-2 bg-amber-300 text-yellow-50 text-sm">Next<i class="ml-2 fa-solid fa-star"></i></div>
       </div>
+
+      <!-- patient counts -->
       <ul class="pb-6 px-6 flex gap-x-2 text-xs">
         <li v-for="count, key in shift.counts" class="mt-2 py-1 px-2 rounded-full uppercase text-xs" :class="countColor(key)">
           {{ key }} 
@@ -72,6 +87,7 @@ function countColor(count) {
           </span>
         </li>
       </ul>
+
     </div>
     <BoardRotationControls v-if="pointer" />
   </BoardPanel>
