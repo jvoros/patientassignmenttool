47b000bc7b969a0d98fd7a8c8851867262824450
Jeremy Voros <jeremyvoros@gmail.com>
2024-07-18 11:42:49 -0600

    new shift times

--- client/public/doctors.js
+++ client/public/doctors.js
@@ -1,8 +1,9 @@
 const doctors = [
   { first: "Alpha Day", last: "6 am", shift: 0 },
-  { first: "Bravo Day", last: "9 am", shift: 1 },
-  { first: "Alpha Swing", last: "3 pm", shift: 2 },
-  { first: "Bravo Swing", last: "4 pm", shift: 3 },
-  { first: "Charlie", last: "7 pm", shift: 4 },
-  { first: "Delta", last: "11 pm", shift: 5 },
+  { first: "Fast Track", last: "6 am", shift: 1 },
+  { first: "Bravo Day", last: "8 am", shift: 2 },
+  { first: "Charlie", last: "11 am", shift: 3 },
+  { first: "Alpha Swing", last: "3 pm", shift: 4 },
+  { first: "Bravo Swing", last: "5 pm", shift: 5 },
+  { first: "Delta", last: "11 pm", shift: 6 },
 ];
